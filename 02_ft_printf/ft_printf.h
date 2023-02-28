a4bb20ca43ab73feba60d9456993c46cc4818e43
Alexandre Brochier <126472981+abrochier93@users.noreply.github.com>
2023-02-27 21:41:59 -0500

    Add files via upload

---
+++ 02_ft_printf/ft_printf.h
@@ -0,0 +1,13 @@
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdarg.h>
+# include <unistd.h>
+
+void	ft_putstr(const char *s, unsigned int *counter);
+void	ft_putnbr(int n, unsigned int *counter);
+void	ft_putunbr(unsigned int n, unsigned int *counter);
+void	ft_puthex(unsigned int hex, char uppercase, unsigned int *counter);
+void	ft_puthexp(unsigned long hex, unsigned int *counter);
+int		ft_printf(const char *str, ...);
+
+#endif

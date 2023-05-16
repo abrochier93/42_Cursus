8e5c908bd97ef8deeafee4d72cc30d3695cd46cc
Alexandre Brochier <126472981+abrochier93@users.noreply.github.com>
2023-05-16 16:52:46 -0400

    Add files via upload

---
+++ 09_philo/src/utils/ft_atoi.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abrochie <abrochie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/16 15:39:46 by abrochie          #+#    #+#             */
+/*   Updated: 2023/05/16 16:19:20 by abrochie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/philo.h"
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	res;
+	int	signe;
+
+	i = 0;
+	signe = 1;
+	res = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || (str[i] == ' '))
+		i++;
+	if (str[i] == '-')
+	{
+		signe *= -1;
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + str[i] - '0';
+		i++;
+	}
+	return (res * signe);
+}
---
+++ 09_philo/src/utils/print.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   print.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abrochie <abrochie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/16 15:41:24 by abrochie          #+#    #+#             */
+/*   Updated: 2023/05/16 15:45:11 by abrochie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/philo.h"
+
+void	print(t_philo *philo, char *str)
+{
+	long int	time;
+
+	pthread_mutex_lock(&(philo->info->print));
+	time = timestamp() - philo->info->t_start;
+	if (!philo->info->stop && time >= 0 \
+			&& time <= INT_MAX && !is_dead(philo, 0))
+		printf("%lld %d %s", timestamp() - philo->info->t_start, philo->n, str);
+	pthread_mutex_unlock(&(philo->info->print));
+}
